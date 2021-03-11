18cba460548a5f564656442e5689ea7de0d35b69
Martin Cifuentes <marcisego@gmail.com>
2021-03-11 06:50:22 -0500

    clase order

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,7 +1,25 @@
 package model;
 
+import java.util.Date;
+
 public class Order {
 	public String code;
+	public State state;
+	public String requestClient;
+	public String deliverEmployee;
+	public Date date;
+	public String observations;
+	public Product[] productsList;
+	public int[] quantityOfProduct;
 	
-	
+	public Order(String c, State s, String rc, String de, Date d, String o, Product[] pl, int[] qp) {
+		this.code = c;
+		this.state = s;
+		this.requestClient = rc;
+		this.deliverEmployee = de;
+		this.date = d;
+		this.observations = o;
+		this.productsList = pl;
+		this.quantityOfProduct = qp;
+	}
 }
