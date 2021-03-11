e1041188a190724e631965791ca6c71783e2a764
unknown <pipe.barreto07@gamil.com>
2021-03-10 19:13:29 -0500

    cambios al diagrama y al ui

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,34 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		
 
+public class Main extends Application {
+
+	private RestaurantGUI restaurantGUI;
+	public Main() {
+		restaurantGUI= new RestaurantGUI();
 	}
-
+	public static void main(String[] args) {
+		launch(args); 
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("main-pane.fxml"));
+		fxmlLoader.setController(restaurantGUI);
+		Parent root= fxmlLoader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("LA CASA DORADA");
+		primaryStage.show();
+		restaurantGUI.loadMainPage();
+		
+	}
 }
---
+++ src/ui/RestaurantGUI.java
@@ -0,0 +1,32 @@
+package ui;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+public class RestaurantGUI {
+
+	public RestaurantGUI() {}
+
+	@FXML
+	private Pane mainPane;
+
+
+	public void loadMainPage(){
+
+		try {
+			FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("main-page.fxml"));
+			fxmlLoader.setController(this);
+			Parent login;
+			login = fxmlLoader.load();
+			mainPane.getChildren().setAll(login);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
